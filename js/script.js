515255d6269cc711d8480c605dc6d8350941b374
Jairo-PC <perez.develoment10@gmail.com>
2023-07-21 19:50:28 -0500

    Actualizacion de portafolio

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Barra navegacion
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e) {
+    document.querySelector('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('fa-xmark')
+})
+
+
+//Efectos
+
+let typed = new Typed('.auto-input', {
+    strings: ['Desarrollador Front-End!', 'Freelancer!', 'Diseñador UI!', 'Youtuber!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+});
