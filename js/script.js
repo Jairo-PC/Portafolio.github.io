f0ddd828abe040cc691274b8ff545acfe2d88a10
Jairo-PC <perez.develoment10@gmail.com>
2023-07-25 16:08:54 -0500

    Actualizacion de codigo

--- js/script.js
+++ js/script.js
@@ -17,3 +17,23 @@ let typed = new Typed('.auto-input', {
     backDelay: 2000,
     loop: true,
 });
+
+//Activar barra de navegación
+
+let navLinks = document.querySelectorAll('nav ul li a')
+
+let sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', function () {
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)) {
+                    link.classList.add('active')
+                }
+            })
+        }
+    })
+})
